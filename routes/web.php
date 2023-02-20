ac0b48d1dcb28e121856e8161c533ca62c2829a9
Leonov Ivan <it.omk@yandex.ru>
2023-02-20 13:27:10 +0300

    Модификация welcom.blade.php и маршрута

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('index');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get(
+    '/home',
+    [HomeController::class, 'index']
+)->name('home');
