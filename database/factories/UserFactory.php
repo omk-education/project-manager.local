4b7162291becdef53a4bd184c129be34a12c9b96
Leonov Ivan <it.omk@yandex.ru>
2023-02-20 13:58:08 +0300

    Модель User + миграция с состояниями и сидами

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' => 'user',
+            'password' => bcrypt('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
@@ -33,8 +34,43 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(
+            fn (array $attributes) => [
+                'email_verified_at' => null,
+            ]
+        );
+    }
+
+    /**
+     * Состояние для Ведущий программист (senior)
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function senior()
+    {
+        return $this->state(
+            function (array $attributes) {
+                return [
+                    'email' => 'senior@test.ru',
+                    'role' => 'senior',
+                ];
+            }
+        );
+    }
+
+    /**
+     * Состояние для программист (junior)
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function junior()
+    {
+        return $this->state(
+            function (array $attributes) {
+                return [
+                    'role' => 'junior',
+                ];
+            }
+        );
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()
+            ->count(1)
+            ->senior()
+            ->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()
+            ->count(2)
+            ->junior()
+            ->create();
+
+        User::factory()
+            ->count(10)
+            ->create();
     }
 }
