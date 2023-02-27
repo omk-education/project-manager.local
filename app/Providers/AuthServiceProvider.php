a3e8cdbc124cdb4e65d960a1ec78530f643024cd
Leonov Ivan <it.omk@yandex.ru>
2023-02-27 13:18:37 +0300

    Gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +21,37 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define(
+            'user',
+            function ($user) {
+                if ($user->role == 'user') {
+                    return true;
+                }
+
+                return false;
+            }
+        );
+
+        Gate::define(
+            'junior',
+            function ($user) {
+                if ($user->role == 'junior') {
+                    return true;
+                }
+
+                return false;
+            }
+        );
+
+        Gate::define(
+            'senior',
+            function ($user) {
+                if ($user->role == 'senior') {
+                    return true;
+                }
+
+                return false;
+            }
+        );
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,9 +4,9 @@
   <div class="container">
     <div class="card">
       <h5 class="card-header">
-        Задачи
+        Пользователи
         <a class="btn btn-outline-success float-end" role="button" aria-disabled="true"
-          href="{{ route('tasks.create') }}">Добавить задачу</a>
+          href="{{ route('users.create') }}">Добавить пользователя</a>
       </h5>
       <div class="card-body">
 
@@ -17,31 +17,20 @@
               <td>{{ $item->id }}</td>
             </tr>
             <tr>
-              <th>Название</th>
+              <th>Имя пользователя</th>
               <td>{{ $item->name }}</td>
             </tr>
             <tr>
-              <th>Описание</th>
-              <td>{{ $item->description }}</td>
+              <th>Роль</th>
+              <td>{{ $item->role }}</td>
             </tr>
-            <tr>
-              <th>Приоритет</th>
-              <td>{{ $item->priority }}</td>
-            </tr>
-            <tr>
-              <th>Исполнитель</th>
-              <td>{{ $item->user->name }}</td>
-            </tr>
-            <tr>
-              <th>Выполнено</th>
-              <td>{{ $item->completed }}</td>
-            </tr>
+
             <tr>
               <th class="col-3">Действия</th>
               <td>
-                <a class="btn btn-outline-primary ml-1" href="{{ route('tasks.edit', $item->id) }}">Редактировать</a>
+                <a class="btn btn-outline-primary ml-1" href="{{ route('users.edit', $item->id) }}">Редактировать</a>
 
-                <form action="{{ route('tasks.destroy', $item->id) }}" method="post" class="float-start mr-1">
+                <form action="{{ route('users.destroy', $item->id) }}" method="post" class="float-start mr-1">
                   @csrf
                   @method('delete')
                   <button type="submit" class="btn btn-outline-danger">Удалить</button>
