93fec5a5693ec3df6533c0e7091b79480c925bed
Leonov Ivan <it.omk@yandex.ru>
2023-02-27 14:11:09 +0300

    Задачи статус

--- resources/views/tasks/partials/form.blade.php
+++ resources/views/tasks/partials/form.blade.php
@@ -1,52 +1,57 @@
-<div class="mb-3">
-  <label for="name" class="form-label">
-    Название задачи
-  </label>
-  <input type="text" class="form-control" id="name" name="name" value="{{ $item->name ?? old('name') }}">
-</div>
+<fieldset @can('junior') disabled @endcan>
+  <div class="mb-3">
+    <label for="name" class="form-label">
+      Название задачи
+    </label>
+    <input type="text" class="form-control" id="name" name="name" value="{{ $item->name ?? old('name') }}">
+  </div>
 
-<div class="mb-3">
-  <label for="description" class="form-label">
-    Описание задачи
-  </label>
-  <textarea class="form-control" id="description" name="description" rows="3">{{ $item->description ?? old('description') }}</textarea>
-</div>
+  <div class="mb-3">
+    <label for="description" class="form-label">
+      Описание задачи
+    </label>
+    <textarea class="form-control" id="description" name="description" rows="3">{{ $item->description ?? old('description') }}</textarea>
+  </div>
 
-<div class="mb-3">
-  <label for="priority" class="form-label">
-    Приоритет задачи
-  </label>
-  <input type="number" class="form-control" id="priority" name="priority"
-    value="{{ $item->priority ?? old('priority') }}">
-</div>
+  <div class="mb-3">
+    <label for="priority" class="form-label">
+      Приоритет задачи
+    </label>
+    <input type="number" class="form-control" id="priority" name="priority"
+      value="{{ $item->priority ?? old('priority') }}">
+  </div>
 
-<div class="mb-3">
-  <label for="user_id" class="form-label">
-    Исполнитель
-  </label>
-  <select class="form-select" name="user_id">
-    @forelse($juniors as $junior)
-      <option value="{{ $junior->id }}" {{ ($item->user_id ?? old('user_id')) == $junior->id ? 'selected' : '' }}>
-        {{ $junior->name }}
+  <div class="mb-3">
+    <label for="user_id" class="form-label">
+      Исполнитель
+    </label>
+    <select class="form-select" name="user_id">
+      @forelse($juniors as $junior)
+        <option value="{{ $junior->id }}" {{ ($item->user_id ?? old('user_id')) == $junior->id ? 'selected' : '' }}>
+          {{ $junior->name }}
+        </option>
+      @empty
+      @endforelse
+    </select>
+  </div>
+
+</fieldset>
+
+<fieldset {{ $item->completed && Auth::user()->can('junior') ? 'disabled' : '' }}>
+  <div class="mb-3">
+    <label for="completed" class="form-label">
+      Статус задачи
+    </label>
+    <select class="form-select" name="completed">
+      <option value="0" {{ ($item->completed ?? old('completed')) == 0 ? 'selected' : '' }}>
+        В работе
       </option>
-    @empty
-    @endforelse
-  </select>
-</div>
+      <option value="1" {{ ($item->completed ?? old('completed')) == 1 ? 'selected' : '' }}>
+        Выполнено
+      </option>
+    </select>
+  </div>
 
-<div class="mb-3">
-  <label for="completed" class="form-label">
-    Статус задачи
-  </label>
-  <select class="form-select" name="completed">
-    <option value="0" {{ ($item->completed ?? old('completed')) == 0 ? 'selected' : '' }}>
-      В работе
-    </option>
-    <option value="1" {{ ($item->completed ?? old('completed')) == 1 ? 'selected' : '' }}>
-      Выполнено
-    </option>
-  </select>
-</div>
-
+</fieldset>
 <hr>
 <button type="submit" class="btn btn-primary">Сохранить</button>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,8 +5,6 @@
     <div class="card">
       <h5 class="card-header">
         Задачи
-        <a class="btn btn-outline-success float-end" role="button" aria-disabled="true"
-          href="{{ route('tasks.create') }}">Добавить задачу</a>
       </h5>
       <div class="card-body">
 
@@ -40,12 +38,6 @@
               <th class="col-3">Действия</th>
               <td>
                 <a class="btn btn-outline-primary ml-1" href="{{ route('tasks.edit', $item->id) }}">Редактировать</a>
-
-                <form action="{{ route('tasks.destroy', $item->id) }}" method="post" class="float-start mr-1">
-                  @csrf
-                  @method('delete')
-                  <button type="submit" class="btn btn-outline-danger">Удалить</button>
-                </form>
               </td>
             </tr>
 
